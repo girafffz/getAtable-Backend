5405637c231be93da92e1dd613180fa16292b927
Steph Luo <steph.luokl@gmail.com>
2022-11-09 11:29:57 +0800

    restaurant_staff routes and api partially completed

--- server.js
+++ server.js
@@ -170,11 +170,24 @@ app.post("/api/restaurants/:id/staff/:staff_id", async (req, res) => {
 // Update a restaurant staff
 app.patch("/api/restaurants/:id/staff/:staff_id", async (req, res) => {
   try {
-    const results = await db.query();
-    console.log(req.params);
+    const results = await db.query(
+      "UPDATE restaurant_staff SET username = $1, name = $2, last_name = $3, email = $4, role = $5, resigned = $6 WHERE restaurant_id = $7 AND id = $8 RETURNING id, username, name, last_name, email, role, resigned",
+      [
+        req.body.username,
+        req.body.name,
+        req.body.last_name,
+        req.body.email,
+        req.body.role,
+        req.body.resigned,
+        req.params.id,
+        req.params.staff_id,
+      ]
+    );
     console.log(results.rows);
     res.status(200).json({
       status: "update successful",
+      results: results.rows[0].name + " " + results.rows[0].last_name,
+      data: { staff: results.rows[0] },
     });
   } catch (error) {
     console.log("PATCH /api/restaurants/:id/staff/:staff_id", error);
@@ -191,8 +204,8 @@ app.patch("/api/restaurants/:id/staff/:staff_id", async (req, res) => {
 app.delete("/api/restaurants/:id/staff/:staff_id", async (req, res) => {
   try {
     const results = await db.query(
-      "DELETE FROM restaurant_staff where id = $1 RETURNING *",
-      [req.params.staff_id]
+      "DELETE FROM restaurant_staff WHERE id = $1 AND restaurant_id = $2 RETURNING *",
+      [req.params.staff_id, req.params.id]
     );
     console.log(results.rows);
     res.status(200).json({
@@ -214,13 +227,16 @@ app.delete("/api/restaurants/:id/staff/:staff_id", async (req, res) => {
 app.put("/api/restaurants/:id/staff", async (req, res) => {
   try {
     // Check if there s a duplicate username
-    const duplicateUsername = await db.query(
-      "SELECT * FROM restaurant_staff WHERE username = $1",
-      [req.body.username]
+    const checkUsername = await db.query(
+      "SELECT EXISTS (SELECT username FROM restaurant_staff WHERE username = $1 AND restaurant_id = $2)",
+      [req.body.username, req.params.id]
     );
 
+    // Storing result returned from username query into another variable
+    const duplicatedUsername = checkUsername.rows[0].exists;
+
     // If username does not exist in database, account will be created
-    if (!duplicateUsername) {
+    if (!duplicatedUsername) {
       const hash = await bcrypt.hash(req.body.password, 12);
       const results = await db.query(
         "INSERT INTO restaurant_staff (username, name, last_name, email, password, role, resigned, restaurant_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
@@ -259,11 +275,29 @@ app.put("/api/restaurants/:id/staff", async (req, res) => {
 });
 
 // Get all restaurant staff
-app.get("/api/restaurants/:id/staff", (req, res) => {
-  console.log(req.body);
-  res.status(200).json({
-    status: "retrieve all successful",
-  });
+app.get("/api/restaurants/:id/staff", async (req, res) => {
+  try {
+    const results = await db.query(
+      "SELECT id, username, name, last_name, email, role, resigned FROM restaurant_staff WHERE restaurant_id = $1",
+      [req.params.id]
+    );
+    console.log(results.rows);
+    res.status(200).json({
+      status: "retrieve all staff successful",
+      total_results: results.rows.length,
+      data: {
+        restaurant_staff: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log("GET /api/restaurants", error);
+    if (error) {
+      res.status(400).json({
+        status: "error",
+        message: "an error has occurred when retrieving staff listing",
+      });
+    }
+  }
 });
 
 ///////////////////////////////////////////////////
