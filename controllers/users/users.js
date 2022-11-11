c9c77f271e5941f83eb6e06ba282be9664011b4a
Steph Luo <steph.luokl@gmail.com>
2022-11-11 14:01:44 +0800

    user login

--- controllers/users/users.js
+++ controllers/users/users.js
@@ -1,5 +1,105 @@
 const db = require("../../db/db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
+
+//////////////////  LOGIN USER  ///////////////////
+const login = async (req, res) => {
+  try {
+    // Find user with the email
+    const findUser = await db.query(
+      "SELECT EXISTS (SELECT email FROM users WHERE email = $1)",
+      [req.body.email]
+    );
+
+    // Storing result returned into another variable
+    const user = findUser.rows[0].exists;
+
+    console.log("user:", user);
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ status: "error", message: "not authorised" });
+    }
+
+    if (user) {
+      const userInfo = await db.query("SELECT * FROM users WHERE email = $1", [
+        req.body.email,
+      ]);
+
+      console.log(userInfo.rows);
+
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        userInfo.rows[0].password
+      ); // parse in 2 things, if matched will return true. Order here is important.
+
+      if (!validPassword) {
+        console.log("username or password error");
+        return res
+          .status(401)
+          .json({ status: "error", message: "login failed" });
+      } else {
+        const payload = {
+          id: userInfo.rows[0].id,
+          email: userInfo.rows[0].email,
+        };
+
+        const access = jwt.sign(payload, process.env.ACCESS_SECRET, {
+          expiresIn: "20m",
+          jwtid: uuidv4(),
+        });
+
+        const refresh = jwt.sign(payload, process.env.REFRESH_SECRET, {
+          expiresIn: "30d",
+          jwtid: uuidv4(),
+        });
+
+        const response = {
+          access,
+          refresh,
+        };
+
+        return res.status(200).json({ status: "successful", response });
+      }
+    }
+  } catch (error) {
+    console.log("POST /api/users/login", error);
+    res.status(400).json({ status: "error", message: "login failed" });
+  }
+};
+
+///////////////////  REFRESH  /////////////////////
+const refresh = (req, res) => {
+  try {
+    const decoded = jwt.verify(req.body.refresh, process.env.REFRESH_SECRET);
+
+    console.log(decoded);
+
+    const payload = {
+      id: decoded.id, // creating the same payload on top
+      email: decoded.email,
+    };
+
+    const access = jwt.sign(payload, process.env.ACCESS_SECRET, {
+      expiresIn: "20m",
+      jwtid: uuidv4(), // for the creation of jti (within jwt's payload section) - helps to link refresh and access tokens
+    });
+
+    const response = {
+      access,
+    };
+
+    res.json(response);
+  } catch (error) {
+    console.log("POST /api/users/refresh", error);
+    res.status(401).json({
+      status: "error",
+      message: "not authorised",
+    });
+  }
+};
 
 /////////////////  GET ONE USER  //////////////////
 const getOneUser = async (req, res) => {
@@ -65,13 +165,15 @@ const createUser = async (req, res) => {
       [req.body.email]
     );
 
+    console.log(checkEmail.rows);
+
     // Storing result returned from user email query into another variable
     const duplicatedEmail = checkEmail.rows[0].exists;
 
     // If user does not exist in database, account will be created
     if (!duplicatedEmail) {
       const hash = await bcrypt.hash(req.body.password, 12);
-      const results = await db.query(
+      const newUser = await db.query(
         "INSERT INTO users (username, name, last_name, email, password, account_active) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, username, name, last_name, email, account_active",
         [
           req.body.username,
@@ -82,10 +184,10 @@ const createUser = async (req, res) => {
           req.body.account_active,
         ]
       );
-      console.log(results.rows);
+
       res.status(200).json({
         status: "create successful",
-        data: { user: results.rows[0] },
+        data: { user: newUser.rows[0] },
       });
     } else {
       res.status(400).json({
@@ -94,7 +196,13 @@ const createUser = async (req, res) => {
       });
     }
   } catch (error) {
-    console.log(error);
+    console.log("PUT /api/users/register", error);
+    if (error) {
+      res.status(400).json({
+        status: "error",
+        message: "an error has occurred",
+      });
+    }
   }
 };
 
@@ -104,7 +212,7 @@ const getAllUsers = async (req, res) => {
     const results = await db.query(
       "SELECT id, username, name, last_name, email, account_active FROM users"
     );
-    console.log(results.rows);
+
     res.status(200).json({
       status: "retrieve all users successful",
       total_results: results.rows.length,
@@ -128,4 +236,6 @@ module.exports = {
   updateUser,
   createUser,
   getAllUsers,
+  login,
+  refresh,
 };
--- routers/users/user.js
+++ routers/users/user.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+// const auth = require("../../middleware/auth");
+
 const {
   getOneUser,
   updateUser,
   createUser,
   getAllUsers,
+  login,
+  refresh,
 } = require("../../controllers/users/users");
 
 ///////////////////  ENDPOINTS  ///////////////////
+router.post("/login", login);
+router.post("/refresh", refresh);
+router.put("/register", createUser);
 router.post("/:user_id", getOneUser);
 router.patch("/:user_id", updateUser);
-router.put("/", createUser);
 router.get("/", getAllUsers);
 
 module.exports = router;
