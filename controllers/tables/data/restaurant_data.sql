ede938f9fadb5cb9f77f72cac566a16180071ebf
Steph Luo <steph.luokl@gmail.com>
2022-11-17 10:24:20 +0800

    Create restaurant_data.sql

---
+++ controllers/tables/data/restaurant_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO restaurants (name, address_line_1, address_line_2, postal_code, website, tel, table_for_2, table_for_4, table_for_6, table_for_8, table_for_10)
+VALUES
+    ('Ramen Shiba', '154 West Coast Road', '#B1-09 West Coast Plaza', '127371', 'https://www.ramenshiba.com/', '84032006', 5, 5, 5, 0, 2),
+    ('Burnt Cones', '105 Clementi Street 12', '#01-02 Sunset Way', '120105', 'https://www.burntcones.com/', '97111097', 8, 5, 2, 0, 0),
+    ('MADLYGOOD', '2 Jurong East Central 1', '#03-12 JCube', '609731', 'https://www.madlygood.com/', '66943235', 0, 10, 0, 4, 0),
+    ('Sichuan Chef', '10 Jurong East Street 12', '#01-04 Jurong East MRT', '609690', NULL, '98925843', 0, 4, 4, 4, 0),
+    ('HoHo Korean Restaurant', '106 Clementi Street 12', '#01-58/60 Sunset Way', '120106', NULL, '62503908', 0, 10, 0, 4, 0),
+    ('Lotus Garden', '1 West Coast Drive', '#01-78 NEWest', '128020', 'http://lotusgarden.com.sg/', '62653718', 3, 5, 3, 0, 2),
+    ('iO Italian Osteria', '4 Hillview Rise', '#02-01 HillV2', '667979', 'http://io-osteria.com/', '67107150', 4, 4, 6, 2, 0),
+    ('Douraku Sushi', '511 Upper Jurong Road', '#01-01 d''Arena', '638366', 'https://www.dourakusushi.com/', '62626996', 0, 10, 0, 0, 0),
+    ('Tuk Tuk Cha', '1 Jurong West Central 2', '#02-K9/K10 Jurong Point', '648886', 'http://www.tuktukcha.com/', NULL, 5, 4, 2, 0, 0),
+    ('Gone Rogue Brewing', '4 Bukit Batok Street 41', '#01-09/10/11 Le Quest Mall', '657991', 'https://www.goneroguebrewing.com/', '91008646', 0, 8, 6, 2, 0),
+    ('Brotzeit', '3 Gateway Drive', '#01-04 Westgate', '608532', 'https://brotzeit.co/', '64659874', 0, 10, 10, 4, 0),
+    ('Fatburger & Buffalo''s', '1 Woodlands Road', '#01-29 Junction 10', '677899', 'https://www.fatburgersg.com/', '62354535', 5, 5, 3, 0, 0);
